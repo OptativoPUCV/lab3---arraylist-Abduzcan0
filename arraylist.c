5433fc594ab2129e8fa3869540a134adf79145fe
Replit user <>
2023-03-30 00:50:56 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -11,18 +11,25 @@ typedef struct ArrayList {
 } ArrayList;
 
 ArrayList *createList(void) {
-ArrayList *list = NULL;
-  list = (ArrayList*) malloc(sizeof(ArrayList));
-  if (list == NULL) exit(EXIT_FAILURE);
-  list->data = (void **) calloc(2, sizeof(void*));
-  list->capacity = 2;
-  list->size = 0;
+    ArrayList *list = NULL;
+    list = (ArrayList*) malloc(sizeof(ArrayList));
+    if (list == NULL) exit(EXIT_FAILURE);
+    list->data = (void **) calloc(2, sizeof(void*));
+    list->capacity = 2;
+    list->size = 0;
 
-  return list;
+    return list;
 }
 
 void append(ArrayList * l, void * data){
-
+    
+    if(l->size==l->capacity){
+        l->capacity*=2;
+        l->data =(void**) realloc(l->data,l->capacity*sizeof(void*));
+        if (l == NULL) exit(EXIT_FAILURE);  
+    }
+    l->data[l->size]=data;
+    l->size++;
 }
 
 void push(ArrayList * l, void * data, int i){
